73684a2bc7caef60476a91d2c80e9f99173e8292
davidlouis101 <54008552+davidlouis101@users.noreply.github.com>
2020-09-19 15:25:55 +0200

    New name for Plugin(Clearinv)

--- src/Commands/Main.php
+++ src/Commands/Main.php
@@ -16,64 +16,27 @@ use pocketmine\entity\Effect;
 class Main extends PluginBase implements Listener{
 
 public function onEnable(){
-$this->getLogger()->info(TextFormat::GREEN . "Commands plugin is now Enabled!");
+$this->getLogger()->info(TextFormat::GREEN . "Clearinventory is now Active");
 }
 
 public function onDisable(){
-$this->getLogger()->info(TextFormat::RED. "Commands plugin is now Disabled!");
+$this->getLogger()->info(TextFormat::RED. "Clearinventory is now Disabled");
 }
 
 public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : bool{
 
 switch($cmd->getName()){
-
-	case "heal":
-		if($sender instanceof Player){
-			if($sender->hasPermission("heal.cmd")){
-				$sender->sendMessage("§9§lServer§7»»§6 You are now healed");
-				$sender->setHealth(20);
-			}
-		}
-		break;
-
-	case "feed":
-		if($sender instanceof Player){
-			if($sender->hasPermission("feed.cmd")){
-				$sender->sendMessage("§9§lServer§7»»§6 You have Succesfully Feed yourself!");
-				$sender->setFood(20);
-			}
-		}
-		break;
 		
        case "clear":
 		if($sender instanceof Player){
 			if($sender->hasPermission("clear.cmd")){
-				$sender->sendMessage("§9§lServer§7»»§7 You have Succesfully Cleared your Inventory.");
+				$sender->sendMessage("§a§lYou Cleared you Inventory and your Armor!");
                                 $sender->getInventory()->clearAll();
 				$sender->getArmorInventory()->clearAll();
 				$sender->removeAllEffects();
 			}
 		}
 		break;
-        
-        case "day":
-		if($sender instanceof Player){
-			if($sender->hasPermission("day.cmd")){
-				$sender->sendMessage("§9§lServer§7»»§7 You set Succesfully the Time to §e6000");
-                                $sender->getLevel()->setTime(6000);
-			}
-		}
-		break;
-
-       case "night":
-		if($sender instanceof Player){
-			if($sender->hasPermission("night.cmd")){
-				$sender->sendMessage("§9§lServer§7»»§6 Time Set to §e16000");
-                                $sender->getLevel()->setTime(16000);
-			}
-		}
-		break;
-
 }
 return true;
 }
