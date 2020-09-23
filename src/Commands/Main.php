e1ae7ad5ec6e8b680590238855d3f002fe3a2c56
davidlouis101 <54008552+davidlouis101@users.noreply.github.com>
2020-09-23 12:47:14 +0200

    Added Sound

--- src/Commands/Main.php
+++ src/Commands/Main.php
@@ -12,12 +12,15 @@ use pocketmine\Player;
 use muqsit\invmenu\{InvMenu, InvMenuHandler};
 use pocketmine\entity\EffectInstance;
 use pocketmine\entity\Effect;
+use pocketmine\network\protocol\LevelEventPacket;
+use pocketmine\network\protocol\LevelSoundEventPacket;
 
 class Main extends PluginBase implements Listener{
 
 public function onEnable(){
 $this->getLogger()->info(TextFormat::GREEN . "Clearinventory is now Active");
 $this->getLogger()->info(TextFormat::BLUE . "Author: Crow Balde");
+$this->getServer()->getPluginManager()->registerEvents($this, $this);
 }
 
 public function onDisable(){
@@ -32,6 +35,8 @@ switch($cmd->getName()){
        case "clear":
 		if($sender instanceof Player){
 			if($sender->hasPermission("clear.cmd")){
+                                $volume = mt_rand();
+			        $sender->getLevel()->broadcastLevelEvent($sender, LevelEventPacket::EVENT_SOUND_ORB, (int) $volume);
 				$sender->sendMessage("§a§lYou Cleared you Inventory and your Armor!");
                                 $sender->getInventory()->clearAll();
 				$sender->getArmorInventory()->clearAll();
