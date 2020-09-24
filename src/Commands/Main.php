a936d783afe335bffb7de131bef5f6fc3e4c1523
davidlouis101 <54008552+davidlouis101@users.noreply.github.com>
2020-09-24 12:50:44 +0200

    Added Config

--- src/Commands/Main.php
+++ src/Commands/Main.php
@@ -13,13 +13,23 @@ use muqsit\invmenu\{InvMenu, InvMenuHandler};
 use pocketmine\entity\EffectInstance;
 use pocketmine\entity\Effect;
 use pocketmine\level\sound\EndermanTeleportSound;
+use pocketmine\utils\Config;
 
 class Main extends PluginBase implements Listener{
+  
+public $myConfig;
 
 public function onEnable(){
 $this->getLogger()->info(TextFormat::GREEN . "Clearinventory is now Active");
 $this->getLogger()->info(TextFormat::BLUE . "Author: Crow Balde");
 $this->getServer()->getPluginManager()->registerEvents($this, $this);
+
+$this->myConfig = (new Config($this->getDataFolder() . "config.yml", Config::YAML, array(
+  "Messages" => [
+    "ClearMessage" => "Setup in Config.yml"
+    "PopupMessage" => "Setup in Config.yml"
+    ]
+  )));
 }
 
 public function onDisable(){
@@ -34,9 +44,16 @@ switch($cmd->getName()){
        case "clear":
 		if($sender instanceof Player){
 			if($sender->hasPermission("clear.cmd")){
-                                $sender->getlevel()->addSound(new EndermanTeleportSound($sender));
-				$sender->sendMessage("§a§lYou Cleared you Inventory and your Armor!");
-                                $sender->getInventory()->clearAll();
+			  //Config
+			  $config = $this->myConfig->getAll();
+				$message = $config["Messages"] ["ClearMessage"];
+				$popup = $config["Messages"] ["PopupMessage"];
+				//sound
+        $sender->getlevel()->addSound(new EndermanTeleportSound($sender));
+        //messages and Clear uses
+				$sender->sendMessage($message);
+				$sender->sendPopup($popup);
+        $sender->getInventory()->clearAll();
 				$sender->getArmorInventory()->clearAll();
 				$sender->removeAllEffects();
 			}
