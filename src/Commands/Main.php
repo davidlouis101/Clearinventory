1880fb06aa8f423f5113832d34a61b9787fd54a2
davidlouis101 <54008552+davidlouis101@users.noreply.github.com>
2020-09-24 18:16:16 +0200

    Update Main.php

--- src/Commands/Main.php
+++ src/Commands/Main.php
@@ -15,18 +15,49 @@ use pocketmine\entity\Effect;
 use pocketmine\level\sound\EndermanTeleportSound;
 use pocketmine\utils\Config;
 
-class Main extends PluginBase implements Listener{
+class Main extends PluginBase{
+  
+public $myConfig;
 
 public function onEnable(){
-$this->getServer()->getPluginManager()->registerEvents($this, $this);
 $this->getLogger()->info(TextFormat::GREEN . "Clearinventory is now Active");
 $this->getLogger()->info(TextFormat::BLUE . "Author: Crow Balde");
+$this->getServer()->getPluginManager()->registerEvents($this, $this);
+
+$this->myConfig = (new Config($this->getDataFolder() . "config.yml", Config::YAML, array(
+  "Messages" => [
+    "ClearMessage" => "Setup in Config.yml"
+    ]
+  )));
 }
 
 public function onDisable(){
-$this->getLogger()->info(TextFormat::GREEN . "Clearinventory is now Offline");
+$this->getLogger()->info(TextFormat::RED. "Clearinventory is now Disabled");
 $this->getLogger()->info(TextFormat::BLUE . "Author: Crow Balde");
+}
+
+public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : bool{
+
+switch($cmd->getName()){
+		
+       case "clear":
+		if($sender instanceof Player){
+			if($sender->hasPermission("clear.cmd")){
+			        //Config
+			        $config = $this->myConfig->getAll();
+				$message = $config["Messages"] ["ClearMessage"];
+				//sound
+                                $sender->getlevel()->addSound(new EndermanTeleportSound($sender));
+                                //messages and Clear uses
+				$sender->sendMessage($message);
+                                $sender->getInventory()->clearAll();
+				$sender->getArmorInventory()->clearAll();
+				$sender->removeAllEffects();
+			}
+		}
+		break;
 }
 return true;
 }
 }
+				  
