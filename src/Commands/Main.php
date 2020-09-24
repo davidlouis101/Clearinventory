14d222187c13b05bde21b1c1343a1b5c827d98fd
davidlouis101 <54008552+davidlouis101@users.noreply.github.com>
2020-09-24 17:54:53 +0200

    Update Main.php

--- src/Commands/Main.php
+++ src/Commands/Main.php
@@ -16,11 +16,16 @@ use pocketmine\level\sound\EndermanTeleportSound;
 use pocketmine\utils\Config;
 
 class Main extends PluginBase implements Listener{
-  
-public $myConfig;
 
 public function onEnable(){
 $this->getServer()->getPluginManager()->registerEvents($this, $this);
+$this->getLogger()->info(TextFormat::GREEN . "Clearinventory is now Active");
+$this->getLogger()->info(TextFormat::BLUE . "Author: Crow Balde");
+}
+
+public function onDisable(){
+$this->getLogger()->info(TextFormat::GREEN . "Clearinventory is now Offline");
+$this->getLogger()->info(TextFormat::BLUE . "Author: Crow Balde");
 }
 return true;
 }
